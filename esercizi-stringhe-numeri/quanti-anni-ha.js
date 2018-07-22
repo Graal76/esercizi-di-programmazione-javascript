81ea074317ad93f93360ad31f2a6ec414ca0c560
Alberto <granata.alberto@gmail.com>
2018-07-22 18:43:24 +0200

    Somma e media risolto

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -3,3 +3,11 @@
   Scrivi un programma che prenda in input un anno di nascita e determini l'età della persona, calcola anche quanti anni sono necessari per raggiungere i 100. Stampa in output entrambi i risultati.
   http://www.imparareaprogrammare.it
 */
+var currentyear = 2018;
+var bornyear = 1982;
+
+var howhold = currentyear - bornyear;
+var yearto100 = 100 - howhold
+
+console.log("Quanti anni hai: ",howhold);
+console.log("quanti anni mancano ai 100?: ",yearto100);
--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -3,3 +3,14 @@
   Scrivi un programma che prenda in input 5 numeri a tua scelta e restituisca in output la somma e la media.
   http://www.imparareaprogrammare.it
 */
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+
+var somma = a+b+c+d+e;
+var media = somma/2;
+
+console.log("Somma: ",somma);
+console.log("Media: ",media);
